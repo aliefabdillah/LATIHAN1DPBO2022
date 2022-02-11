fd5fe806adee0915c0bb7c4dfa4033cbae4fcd12
Alief Muhammad Abdillah <91062027+aliefabdillah@users.noreply.github.com>
2022-02-11 10:57:28 +0700

    add folder latihanpraktikum1b_php

---
+++ latihanpraktikum1b_php/indexTim.php
@@ -0,0 +1,29 @@
+<?php
+
+include "DataTim.php";
+
+//memasukan data ke dalam object
+echo "----------------------". "<br/>";
+$arrData1 = new DataTim("Persib", "Indonesia", "Bandung", "GBLA", "1933", "Robert", "Victor");
+// $arrData1->printHasil();
+echo "Nama : ". $arrData1->getNama(). "<br/>";
+echo "Negara : ". $arrData1->getNegara(). "<br/>";
+echo "Kota : ". $arrData1->getKota(). "<br/>";
+echo "Kandang : ". $arrData1->getKandang(). "<br/>";
+echo "Tahun Berdiri : ". $arrData1->getTahunBerdiri(). "<br/>";
+echo "Pelatih : ". $arrData1->getPelatih(). "<br/>";
+echo "Kapten : ". $arrData1->getKaptenTim(). "<br/>";
+echo "----------------------". "<br/>";
+$arrData2 = new DataTim("Manchester United", "England", "Manchester", "Old Trafford", "1878", "Ragnick", "Maguiare");
+// $arrData2->printHasil();
+echo "Nama : ". $arrData2->getNama(). "<br/>";
+echo "Negara : ". $arrData2->getNegara(). "<br/>";
+echo "Kota : ". $arrData2->getKota(). "<br/>";
+echo "Kandang : ". $arrData2->getKandang(). "<br/>";
+echo "Tahun Berdiri : ". $arrData2->getTahunBerdiri(). "<br/>";
+echo "Pelatih : ". $arrData2->getPelatih(). "<br/>";
+echo "Kapten : ". $arrData2->getKaptenTim(). "<br/>";
+echo "----------------------". "<br/>";
+
+
+?>
